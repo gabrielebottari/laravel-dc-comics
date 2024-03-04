f133e31ae20f134e27d373d6ba5c2c27929bba84
gabrielebottari <gabriele.bottari1@gmail.com>
2024-03-04 15:02:13 +0100

    ADD BONUS italian custom message W/ FormRequest

--- app/Http/Requests/UpdateComicRequest.php
+++ app/Http/Requests/UpdateComicRequest.php
@@ -36,4 +36,32 @@ class UpdateComicRequest extends FormRequest
             'writers' => 'nullable|string',
         ];
     }
+
+    /**
+     * Get custom messages for validator errors.
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            'title.required' => 'Il titolo è obbligatorio.',
+            'title.max' => 'Il titolo non può superare i 255 caratteri.',
+            'description.required' => 'La descrizione è obbligatoria.',
+            'thumb.required' => 'Il campo thumb è obbligatorio.',
+            'thumb.url' => 'Il thumb deve essere un URL valido.',
+            'price.required' => 'Il prezzo è obbligatorio.',
+            'price.numeric' => 'Il prezzo deve essere un numero.',
+            'series.required' => 'La serie è obbligatoria.',
+            'series.max' => 'La serie non può superare i 255 caratteri.',
+            'sale_date.required' => 'La data di vendita è obbligatoria.',
+            'sale_date.date' => 'La data di vendita deve essere una data valida.',
+            'type.required' => 'Il tipo è obbligatorio.',
+            'type.max' => 'Il tipo non può superare i 255 caratteri.',
+            'artists.string' => 'Gli artisti devono essere una stringa.',
+            'artists.nullable' => 'Il campo artisti può essere vuoto.',
+            'writers.string' => 'Gli scrittori devono essere una stringa.',
+            'writers.nullable' => 'Il campo scrittori può essere vuoto.',
+        ];
+    }
 }
--- resources/views/comics/edit.blade.php
+++ resources/views/comics/edit.blade.php
@@ -26,48 +26,93 @@
                 @method('PUT')
 
                 <div class="mb-3">
-                    <label for="title" class="form-label">Title <span class="text-danger">*</span></label>
-                    <input id="title" class="form-control" name="title" placeholder="Title" type="text" value="{{ $comic->title }}">
+                    <label for="title" class="form-label @error('title') is-invalid @enderror">Title <span class="text-danger">*</span></label>
+                    <input id="title" class="form-control" name="title" placeholder="Title" type="text" value="{{ old('title', $comic->title) }}">
+                    @error('title')
+                        <div class="alert alert-danger">
+                            {{ $message }}
+                        </div>
+                    @enderror
                 </div>
 
                 <div class="mb-3">
-                    <label for="series" class="form-label">Series <span class="text-danger">*</span></label>
-                    <input id="series" class="form-control" name="series" placeholder="Series" type="text" value="{{ $comic->series }}">
+                    <label for="series" class="form-label @error('series') is-invalid @enderror">Series <span class="text-danger">*</span></label>
+                    <input id="series" class="form-control" name="series" placeholder="Series" type="text" value="{{ old('series', $comic->series) }}">
+                    @error('series')
+                        <div class="alert alert-danger">
+                            {{ $message }}
+                        </div>
+                    @enderror
                 </div>
 
                 <div class="mb-3">
-                    <label for="type" class="form-label">Type <span class="text-danger">*</span></label>
-                    <input id="type" class="form-control" name="type" placeholder="Type" type="text" value="{{ $comic->type }}">
+                    <label for="type" class="form-label @error('type') is-invalid @enderror">Type <span class="text-danger">*</span></label>
+                    <input id="type" class="form-control" name="type" placeholder="Type" type="text" value="{{ old('type', $comic->type) }}">
+                    @error('type')
+                        <div class="alert alert-danger">
+                            {{ $message }}
+                        </div>
+                    @enderror
                 </div>
 
                 <div class="mb-3">
-                    <label for="sale_date" class="form-label">Sale Date <span class="text-danger">*</span></label>
-                    <input type="date" id="sale_date" class="form-control" name="sale_date" placeholder="Sale Date" type="text" value="{{ $comic->sale_date }}">
+                    <label for="sale_date" class="form-label @error('sale_date') is-invalid @enderror">Sale Date <span class="text-danger">*</span></label>
+                    <input type="date" id="sale_date" class="form-control" name="sale_date" placeholder="Sale Date" type="text" value="{{ old('sale_date', $comic->sale_date) }}">
+                    @error('sale_date')
+                        <div class="alert alert-danger">
+                            {{ $message }}
+                        </div>
+                    @enderror
                 </div>
 
                 <div class="mb-3">
-                    <label for="price" class="form-label">Price <span class="text-danger">*</span></label>
-                    <input id="price" class="form-control" name="price" placeholder="Price" type="text" value="{{ $comic->price }}">
+                    <label for="price" class="form-label @error('price') is-invalid @enderror">Price <span class="text-danger">*</span></label>
+                    <input id="price" class="form-control" name="price" placeholder="Price" type="text" value="{{ old('price', $comic->price) }}">
+                    @error('price')
+                        <div class="alert alert-danger">
+                            {{ $message }}
+                        </div>
+                    @enderror
                 </div>
 
                 <div class="mb-3">
-                    <label for="thumb" class="form-label">Thumb</label>
-                    <input id="thumb" class="form-control" name="thumb" placeholder="Thumb" type="text" value="{{ $comic->thumb }}">
+                    <label for="thumb" class="form-label @error('thumb') is-invalid @enderror">Thumb</label>
+                    <input id="thumb" class="form-control" name="thumb" placeholder="Thumb" type="text" value="{{ old('thumb', $comic->thumb) }}">
+                    @error('thumb')
+                        <div class="alert alert-danger">
+                            {{ $message }}
+                        </div>
+                    @enderror
                 </div>
 
                 <div class="mb-3">
-                    <label for="description" class="form-label">Description</label>
-                    <textarea class="form-control"  name="description" id="description" cols="30" rows="10" placeholder="Description" value="{{ $comic->description }}">{{ $comic->description }}</textarea>
+                    <label for="description" class="form-label @error('description') is-invalid @enderror">Description</label>
+                    <textarea class="form-control"  name="description" id="description" cols="30" rows="10" placeholder="Description" value="{{ old('description', $comic->description) }}">{{ old('description', $comic->description) }}</textarea>
+                    @error('description')
+                        <div class="alert alert-danger">
+                            {{ $message }}
+                        </div>
+                    @enderror
                 </div>
 
                 <div class="mb-3">
-                    <label for="artists" class="form-label">Artists</label>
-                    <input id="artists" class="form-control" name="artists" placeholder="Artists" type="text" value="{{ $comic->artists }}">
+                    <label for="artists" class="form-label @error('artists') is-invalid @enderror">Artists</label>
+                    <input id="artists" class="form-control" name="artists" placeholder="Artists" type="text" value="{{ old('artists', $comic->artists) }}">
+                    @error('artists')
+                        <div class="alert alert-danger">
+                            {{ $message }}
+                        </div>
+                    @enderror
                 </div>
 
                 <div class="mb-3">
-                    <label for="writers" class="form-label">Writers</label>
-                    <input id="writers" class="form-control" name="writers" placeholder="Writers" type="text" value="{{ $comic->writers }}">
+                    <label for="writers" class="form-label @error('writers') is-invalid @enderror">Writers</label>
+                    <input id="writers" class="form-control" name="writers" placeholder="Writers" type="text" value="{{ old('writers', $comic->writers) }}">
+                    @error('writers')
+                        <div class="alert alert-danger">
+                            {{ $message }}
+                        </div>
+                    @enderror
                 </div>
 
                 <div class="d-flex justify-content-between">
